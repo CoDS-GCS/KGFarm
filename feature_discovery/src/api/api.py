50befe2dc77192a8698c542c7a521a5cc4d2db16
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-05-10 16:58:15 -0400

    refactor apis

--- feature_discovery/src/api/api.py
+++ feature_discovery/src/api/api.py
@@ -1,79 +1,115 @@
+import os
+
 import pandas as pd
 import numpy as np
 from feature_discovery.src.api.template import *
 from helpers.helper import *
-from helpers.feast_templates import entity_skeleton, feature_view, definitions
+from helpers.feast_templates import entity_skeleton, feature_view_skeleton, definitions
 
-# TODO: populate entities and feature_views while initializing
 
 class KGFarm:
-    def __init__(self, blazegraph_port=9999, blazegraph_namespace: str = 'glac', show_connection_status: bool = True):
+    def __init__(self, blazegraph_port=9999, blazegraph_namespace: str = 'glac',
+                 path_to_feature_repo: str = 'feature_repo/', show_connection_status: bool = True):
+        # remove old feast meta
+        if os.path.exists(path_to_feature_repo + 'data/registry.db'):
+            os.remove(path_to_feature_repo + 'data/registry.db')
+        if os.path.exists(path_to_feature_repo + 'data/online_store.db'):
+            os.remove(path_to_feature_repo + 'data/online_store.db')
+        if os.path.exists(path_to_feature_repo + 'data/driver_stats.parquet'):
+            os.remove(path_to_feature_repo + 'data/driver_stats.parquet')
+
+        self.path_to_feature_repo = path_to_feature_repo
         self.config = connect_to_blazegraph(blazegraph_port, blazegraph_namespace, show_connection_status)
-        self.entities = {}
-        self.feature_views = {}
-    # def predict_entities_and_feature_views(self, ttl: int = 1000, thresh=0.70, show_query=False):
-    #
-    #     def create_file(df_feast: pd.DataFrame, path: str = 'feature_repo/'):
-    #         entities = df_feast[['Source_table', 'Source_column', 'Source_column_type', 'Source_table_path']] \
-    #             .drop_duplicates()
-    #         feast_driver_file = definitions()
-    #         count = 1
-    #         for index, row in entities.iterrows():
-    #             e = entity().format(count, row['Source_column'], row['Source_column_type'], row['Source_column'])
-    #             f = feature_view().format(count, count, row['Source_column'], ttl, row['Source_table_path'])
-    #             count = count + 1
-    #             feast_driver_file = feast_driver_file + e + f
-    #
-    #         with open(path + 'predicted_register.py', 'w') as py_file:
-    #             py_file.write(feast_driver_file)
-    #             print('Predicted entities and feature view(s) File saved at: ', path + 'predicted_register.py')
-    #
-    #     df = pd.DataFrame(predict_entities_and_feature_views(self.config, thresh, show_query)).\
-    #         replace(['N', 'T'], ['INT64', 'STRING'])
-    #     create_file(df)
-    #     return df
+        self.entities_to_feature_views = {}
+        self.entities, self.feature_views = self.__predict_entities_and_feature_views()
 
-    def predict_entities_and_feature_views(self, ttl: int = 1000, path_to_feature_repo: str = 'feature_repo/', show_feature_views: bool = False, show_query: bool = False):
-        entities, self.feature_views, entities_df = predict_entities(self.config, show_query)
-        if os.path.exists(path_to_feature_repo + 'predicted_register.py'):
-            os.remove(path_to_feature_repo + 'predicted_register.py')
+    # predicts the default feature views and entities at the time of initialization
+    def __predict_entities_and_feature_views(self, ttl: int = 10, show_query: bool = False):
+        entities = {}
+        feature_views = {}
+        entities_df = predict_entities(self.config, show_query)
+        # add feature_views corresponding to each entity (here entity = primary key extracted from KG)
+        entities_df.insert(loc=0, column='Feature_view',
+                           value=list('feature_view_' + str(i) for i in range(1, len(entities_df) + 1)))
 
-        with open(path_to_feature_repo + 'predicted_register.py', 'a') as py_file:
+        # convert to dictionary
+        for k, v in entities_df.to_dict('index').items():
+            entities[v['Entity']] = {'Entity_data_type': v['Entity_data_type']}
+
+            feature_views[v['Feature_view']] = {'Entity': v['Entity'], 'Entity_data_type': v['Entity_data_type'],
+                                                'Time_to_leave': ttl,
+                                                'File_source': v['File_source'],
+                                                'File_source_path': v['File_source_path'],
+                                                'Dataset': v['Dataset']}
+            self.entities_to_feature_views[v['Entity']] = v['Feature_view']
+        return entities, feature_views
+
+    def show_entities(self):
+        return convert_dict_to_dataframe('Entity', self.entities)
+
+    def show_feature_views(self):
+        return convert_dict_to_dataframe('Feature_view', self.feature_views)
+
+    # writes to file the predicted feature views and entities
+    def predict_feature_views(self, ttl: int = 10,
+                              show_feature_views: bool = False):
+        if os.path.exists(self.path_to_feature_repo + 'predicted_register.py'):
+            os.remove(self.path_to_feature_repo + 'predicted_register.py')
+        if os.path.exists(self.path_to_feature_repo + 'example.py'):
+            os.remove(self.path_to_feature_repo + 'example.py')
+
+        # update feature views
+        for value in self.feature_views.values():
+            value['Time_to_leave'] = ttl
+
+        # write to file
+        with open(self.path_to_feature_repo + 'predicted_register.py', 'a') as py_file:
+            # write basic library imports + documentation
             py_file.write(definitions())
-            count = 0
-            for k, v in entities.items():
-                e = entity_skeleton().format(v['name'], v['name'], v['datatype'], v['name'])
-                py_file.write(e)
-                for file_source in v['FileSource_path']:
-                    count = count + 1
-                    f = feature_view().format(count, count, v['name'], ttl, file_source)
-                    py_file.write(f)
+            # write all entities
+            for entity, v in self.entities.items():
+                entity = entity_skeleton().format(entity, entity, v['Entity_data_type'], entity)
+                py_file.write(entity)
+            # write all feature views
+            for feature_view, v in self.feature_views.items():
+                feature_view = feature_view_skeleton().format(feature_view, feature_view, v['Entity'], ttl,
+                                                              v['File_source_path'])
+                py_file.write(feature_view)
             py_file.close()
-            print('Predicted entities and feature view(s) File saved at: ', os.path.abspath(path_to_feature_repo) + '/predicted_register.py\n')
-
+        print('Predicted entities and feature view(s) File saved at: ',
+              os.path.abspath(self.path_to_feature_repo) + '/predicted_register.py\n')
         if show_feature_views:
-            print('Showing predicted Feature views:')
-            cols = entities_df.columns.tolist()
-            cols = cols[-1:] + cols[:-1]
-            return entities_df[cols]
+            return self.show_feature_views()
 
-    def predict_features(self, table_info: pd.Series, show_query: bool = False):
-        table = table_info['table_name']
-        dataset = table_info['dataset_name']
-        # print('table: ', table)
-        joinable_table, cols = predict_features(self.config, table, dataset, show_query)
-        # print('joinable table: ', joinable_table)
-        fv = ''
-        for k, v in self.feature_views.items():
-            if joinable_table == self.feature_views.get(k):
-                fv = k
-        predicted_features = []
-        for c in cols:
-            predicted_features.append(fv+':'+c)
-        return predicted_features
+    def get_enrichment_tables(self, show_query: bool = False):
+        df = get_enrichment_tables(self.config, show_query)
+        df['Enrich_with'] = df['Entity'].apply(lambda x: self.entities_to_feature_views.get(x))
+        df['File_source_path'] = df['Enrich_with'].apply(lambda x: self.feature_views.get(x)['File_source_path'])
+        df['Dataset_feature_view'] = df['Enrich_with'].apply(lambda x: self.feature_views.get(x)['Dataset'])
+        df['File_source'] = df['Enrich_with'].apply(lambda x: self.feature_views.get(x)['File_source'])
+        df = df.drop('Entity', axis=1)
+        # rearrange columns
+        # df = df[df.columns.tolist()[-1:] + df.columns.tolist()[:-1]]
+        df = df[['Table', 'Enrich_with', 'Path_to_table', 'File_source_path', 'File_source', 'Dataset', 'Dataset_feature_view']]
+        return df.sort_values('Enrich_with')
+
+    # def predict_features(self, table_info: pd.Series, show_query: bool = False):
+    #     table = table_info['table_name']
+    #     dataset = table_info['dataset_name']
+    #     # print('table: ', table)
+    #     joinable_table, cols = predict_features(self.config, table, dataset, show_query)
+    #     # print('joinable table: ', joinable_table)
+    #     fv = ''
+    #     for k, v in self.feature_views.items():
+    #         if joinable_table == self.feature_views.get(k):
+    #             fv = k
+    #     predicted_features = []
+    #     for c in cols:
+    #         predicted_features.append(fv + ':' + c)
+    #     return predicted_features
 
 
 if __name__ == "__main__":
-    kgfarm = KGFarm(show_connection_status=False)
-    df = kgfarm.predict_entities_and_feature_views(path_to_feature_repo='../../../feature_repo/', show_query=False)
-    print(df)
+    kgfarm = KGFarm(path_to_feature_repo='../../../feature_repo/', show_connection_status=False)
+    print(kgfarm.predict_feature_views(ttl=10, show_feature_views=True))
+    print(kgfarm.get_enrichment_tables())
--- feature_discovery/src/api/template.py
+++ feature_discovery/src/api/template.py
@@ -1,5 +1,6 @@
-from helpers.helper import execute_query_blazegraph, execute_query
+from helpers.helper import execute_query_blazegraph, execute_query, display_query
 import numpy as np
+
 PREFIXES = """
     PREFIX lac:     <http://www.example.com/lac#>
     PREFIX schema:  <http://schema.org/>
@@ -24,90 +25,67 @@ def get_columns(config, table, dataset):
     return execute_query(config, query)['Column'].tolist()
 
 
-
-
-def predict_entities_and_feature_views(config, thresh, show_query):
-    query = PREFIXES + """
-    SELECT DISTINCT ?Source_table ?Source_column ?Source_column_type ?Source_table_path ?Target_table ?Target_column ?Certainty_score
-    WHERE
-    {
-      <<?column_id lac:pkfk ?column_id_2>> lac:certainty	?Certainty_score	.
-      FILTER (?Certainty_score >= %s)                        					.
-      ?column_id    schema:name	    ?Source_column								.
-      ?column_id    schema:type     ?Source_column_type     					.
-      ?column_id_2	schema:name	    ?Target_column								.
-      ?column_id    dct:isPartOf	?table_id									.
-      ?table_id     lac:path       	?Source_table_path  						.
-      ?column_id_2	dct:isPartOf	?table_id_2									.
-      ?table_id		schema:name		?Source_table								.
-      ?table_id_2	schema:name		?Target_table								.
-    }""" % thresh
-
-    if show_query:
-        print(query)
-
-    results = execute_query_blazegraph(config, query)["results"]["bindings"]
-    for result in results:
-        yield {'Source_table': result['Source_table']['value'],
-               'Source_column': result['Source_column']['value'],
-               'Source_column_type': result['Source_column_type']['value'],
-               'Source_table_path': result['Source_table_path']['value'],
-               'Target_table': result['Target_table']['value'],
-               'Target_column': result['Target_column']['value'],
-               'Certainty_score': result['Certainty_score']['value']}
-
-
 def predict_entities(config, show_query):
-    query = PREFIXES + """
-    SELECT DISTINCT ?Entity_id ?Entity ?Entity_data_type ?FileSource ?Joinable_table ?FileSource_path 
+    query = """
+    SELECT DISTINCT ?Entity ?Entity_data_type ?File_source ?File_source_path ?Dataset
     WHERE
     {
-      <<?Entity_id  lac:pkfk ?Column_2>>	lac:certainty	?Score	.
-      ?Entity_id	schema:name				?Entity					.
-      ?Entity_id	dct:isPartOf			?Joinable_table_id		.
-      ?Joinable_table_id	schema:name		?Joinable_table			.
-      ?Column_2	    dct:isPartOf			?Table_id				.	
-      ?Table_id	    schema:name				?FileSource				;
-                    lac:path                ?FileSource_path        .
-      ?Entity_id	schema:type				?Entity_data_type		.	
-      ?Entity_id	schema:totalVCount		?Total_values			.
-      ?Entity_id	schema:distinctVCount	?Distinct_values		.
+      <<?Entity_id 	lac:pkfk ?column_id_1>> lac:certainty ?score	.
+      ?Entity_id	schema:totalVCount		?Total_values			;
+      				schema:distinctVCount	?Distinct_values		;
+                    schema:name				?Entity					;
+      				schema:type				?Entity_data_type		.
+      
       FILTER(?Total_values = ?Distinct_values)					    .
-    }
+      
+      ?Entity_id	dct:isPartOf			?Table_id				.
+      ?Table_id     schema:name             ?File_source            ;
+                    lac:path				?File_source_path       ;
+                    dct:isPartOf            ?Dataset_id             .
+      
+      ?Dataset_id   schema:name             ?Dataset               .
+    }"""
+    if show_query:
+        display_query(query)
+
+    entities = execute_query(config, query). \
+        replace(['N', 'T'], ['INT64', 'STRING'])
+    return entities
+
+
+def get_enrichment_tables(config, show_query):
+    query = """
+    SELECT DISTINCT ?Table ?Entity ?Path_to_table ?Dataset
+    WHERE
+    {
+      <<?Entity_id 			lac:pkfk ?column_id_1>> lac:certainty ?Score	.
+      ?Entity_id			schema:totalVCount		?Total_values			;
+      						schema:distinctVCount	?Distinct_values		;
+                    		schema:name				?Entity					;
+      						schema:type				?Entity_data_type		;
+                    		dct:isPartOf			?Table_id				.
+      
+      FILTER(?Total_values = ?Distinct_values)					    		.
+      	
+      ?Table_id     		schema:name             ?File_source            ;
+      						lac:path				?File_source_path       .
+      
+      ?column_id_1			dct:isPartOf			?Joinable_table_id		.
+      ?Joinable_table_id	schema:name				?Table      			;
+                            lac:path				?Path_to_table			;
+                            dct:isPartOf            ?Dataset_id             .
+                
+      ?Dataset_id           schema:name             ?Dataset                .            
+      FILTER(?Table_id != ?Joinable_table_id)					    		.
+    } ORDER BY DESC (?Entity)
     """
     if show_query:
-        print(query)
+        display_query(query)
 
-    results = execute_query_blazegraph(config, query)["results"]["bindings"]
-    entities = {}
-    for result in results:
-        datatype = result['Entity_data_type']['value']
-        if datatype == 'N':
-            datatype = 'INT64'
-        elif datatype == 'T':
-            datatype = 'STRING'
-        if result['Entity_id']['value'] in entities:
-            file_source_path = entities.get(result['Entity_id']['value'])['FileSource_path']
-            file_source_path.append(result['FileSource_path']['value'])
-            entities[result['Entity_id']['value']] = {'name': result['Entity']['value'], 'datatype': datatype,
-                                                      'FileSource_path': file_source_path}
-        else:
-            entities[result['Entity_id']['value']] = {'name': result['Entity']['value'], 'datatype': datatype,
-                                                      'FileSource_path': [result['FileSource_path']['value']]}
-
-    entities_df = execute_query(config, query).drop(['Entity_id'], axis=1). \
-        replace(['N', 'T'], ['INT64', 'STRING'])
-    entities_df['Feature_view'] = ['feature_view_' + str(i) for i in range(1, np.shape(entities_df)[0] + 1)]
-
-    feature_views = {}
-    feature_views_raw = entities_df.to_dict('index')
-    for k, v in feature_views_raw.items():
-        feature_views['predicted_' + feature_views_raw.get(k)['Feature_view']] = feature_views_raw.get(k)['FileSource']
-
-    return entities, feature_views, entities_df
+    return execute_query(config, query)
 
 
-# TODO: add support for all predicted joinable tables for kgarm.predict_features()
+# TODO: add support for all predicted joinable tables for kgfarm.predict_features()
 def predict_features(config, table, dataset, show_query):
     query = PREFIXES + """
     SELECT DISTINCT ?Source_table ?Joinable_table (?Column_id as ?Join_key_id)
@@ -127,5 +105,6 @@ def predict_features(config, table, dataset, show_query):
     df = execute_query(config, query)
     joinable_table = df['Joinable_table'].iloc[0]
     get_columns(config, joinable_table, dataset)
-    columns_to_join = [i for i in get_columns(config, joinable_table, dataset) if i not in get_columns(config, table, dataset)]
+    columns_to_join = [i for i in get_columns(config, joinable_table, dataset) if
+                       i not in get_columns(config, table, dataset)]
     return joinable_table, columns_to_join
