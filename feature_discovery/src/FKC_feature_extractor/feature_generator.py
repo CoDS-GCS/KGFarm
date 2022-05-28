faf9b082d489bdd4bbebe12d2005cb9f38abfd07
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-05-28 15:00:17 -0400

    add F3, F4 and F5 generation

--- feature_discovery/src/FKC_feature_extractor/feature_generator.py
+++ feature_discovery/src/FKC_feature_extractor/feature_generator.py
@@ -7,19 +7,70 @@ import pandas as pd
 def generate_features(conn, ind: pd.DataFrame):
     def aggregate_features(ind_pairs: pd.DataFrame, features: list):
         # 1. join features, key = A & B
-        # 2. if NAN i.e. edge didn't existed --> 0
+        # 2. if NAN i.e. edge didn't exist --> 0
         # 3. repeat for all features
         result = ind_pairs
         for feature in tqdm(features):
             result = pd.concat([result, feature], axis=1)
             result = result.loc[:, ~result.columns.duplicated()].fillna(0)
+
         return result
+
+    def normalize(vector: list):
+        max_value = max(vector)
+        return list(map(lambda x: x/max_value, vector))
 
     def generate_F01():
         return get_distinct_dependent_values(conn)
 
     def generate_F02():
         return get_content_similarity(conn)
+
+    def generate_F03():
+        # TODO: F3 generation needs optimization
+        # counts how often A appears as B
+        count = []
+        A = ind['A'].to_list()
+        B = ind['B'].tolist()
+
+        for a in A:
+            presence = 0
+            for b in B:
+                if a == b:
+                    presence = presence + 1
+            count.append(presence)
+
+        count = normalize(count)
+        f3 = ind
+        f3['F3'] = count
+        return f3
+
+    def generate_F04():
+        A = ind['A'].tolist()
+        freq = list(map(lambda x: A.count(x), A))
+        freq = normalize(freq)
+        f4 = ind
+        f4['F4'] = freq
+        return f4
+
+    def generate_F05():
+        # TODO: F5 generation needs optimization
+        # counts how often B appears as A
+        count = []
+        A = ind['A'].to_list()
+        B = ind['B'].tolist()
+
+        for b in B:
+            presence = 0
+            for a in B:
+                if a == b:
+                    presence = presence + 1
+            count.append(presence)
+
+        count = normalize(count)
+        f5 = ind
+        f5['F5'] = count
+        return f5
 
     def generate_F06():
         return get_column_name_similarity(conn)
@@ -30,20 +81,25 @@ def generate_features(conn, ind: pd.DataFrame):
     def generate_F10():
         return get_table_size_ratio(conn)
 
+    generate_F04()
     return aggregate_features(ind, [generate_F01(),
                                     generate_F02(),
+                                    generate_F03(),
+                                    generate_F04(),
+                                    generate_F05(),
                                     generate_F06(),
                                     generate_F09(),
-                                    generate_F10()])
+                                    generate_F10()])[['A', 'B', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F9', 'F10']]
 
 
 def main():
     # This implementation is as per http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.438.9288&rep=rep1&type=pdf
-    conn = connect_to_stardog(port=5822, db='chembl', show_status=True)
+    conn = connect_to_stardog(port=5820, db='chembl', show_status=True)
     # Get all inclusion dependencies (IND) initially
     ind = get_INDs(conn)
     # Generate features for these IND pairs
     features_df = generate_features(conn, ind)
+    # print(features_df)
     features_df.to_csv('features.csv')
 
 
---
+++ feature_repo/predicted_register.py
@@ -0,0 +1,138 @@
+# The Entities & Feature Views were predicted using Discovery operations. Feel free to edit :)
+from datetime import timedelta
+from feast import Entity, FeatureView, FileSource, ValueType
+'''
+####################################################################################################################
+Entity: 
+        An entity is the object on which features are observed.
+        Used when joining feature values n order to build one large data set.
+--------------------------------------------------------------------------------------------------------------------
+Feature View: 
+        A feature view is an object that represents a logical group of feature data as it is found in a data source.
+####################################################################################################################
+'''
+district_id = Entity(name='district_id', value_type=ValueType.INT64, join_key='district_id')
+
+account_id = Entity(name='account_id', value_type=ValueType.STRING, join_key='account_id')
+
+client_id = Entity(name='client_id', value_type=ValueType.INT64, join_key='client_id')
+
+feature_view_1 = FeatureView(
+name='feature_view_1',
+entities=['district_id'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completeddistrict.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_2 = FeatureView(
+name='feature_view_2',
+entities=['account_id'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedacct.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_3 = FeatureView(
+name='feature_view_3',
+entities=['client_id'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completeddisposition.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_4 = FeatureView(
+name='feature_view_4',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/crm_call_center_logs.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_5 = FeatureView(
+name='feature_view_5',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/account.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_6 = FeatureView(
+name='feature_view_6',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedloan.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_7 = FeatureView(
+name='feature_view_7',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/luxuryloanportfolio.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_8 = FeatureView(
+name='feature_view_8',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/crm_reviews.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_9 = FeatureView(
+name='feature_view_9',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedclient.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_10 = FeatureView(
+name='feature_view_10',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/district.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_11 = FeatureView(
+name='feature_view_11',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedorder.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+feature_view_12 = FeatureView(
+name='feature_view_12',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedcard.parquet',
+event_timestamp_column='event_timestamp')
+)
