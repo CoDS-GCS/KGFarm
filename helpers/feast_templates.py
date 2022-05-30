550bf206d0701045e7d8eed14189c6c412b7dcb6
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-05-30 16:48:12 -0400

    update feast template

--- helpers/feast_templates.py
+++ helpers/feast_templates.py
@@ -7,7 +7,7 @@ from feast import Entity, FeatureView, FileSource, ValueType
 ####################################################################################################################
 Entity: 
         An entity is the object on which features are observed.
-        Used when joining feature values n order to build one large data set.
+        Used when joining feature values in order to build one large data set.
 --------------------------------------------------------------------------------------------------------------------
 Feature View: 
         A feature view is an object that represents a logical group of feature data as it is found in a data source.
