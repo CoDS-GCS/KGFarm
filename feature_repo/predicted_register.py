a34cb76468db967d8f9600e7e087a31c8cb344db
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-06-25 21:23:32 -0400

    revise demo for fixed builder

--- feature_repo/predicted_register.py
+++ feature_repo/predicted_register.py
@@ -13,64 +13,26 @@ Feature View:
 '''
 account__completedacct = Entity(name='account__completedacct', value_type=ValueType.STRING, join_key='account_id')
 
-card__completedcard = Entity(name='card__completedcard', value_type=ValueType.STRING, join_key='card_id')
-
-loan__luxuryloanportfolio = Entity(name='loan__luxuryloanportfolio', value_type=ValueType.STRING, join_key='social')
-
 account__completedloan = Entity(name='account__completedloan', value_type=ValueType.STRING, join_key='account_id')
+
+client__completedclient = Entity(name='client__completedclient', value_type=ValueType.STRING, join_key='email')
+
+client__completeddisposition = Entity(name='client__completeddisposition', value_type=ValueType.INT64, join_key='client_id')
+
+disp__completedcard = Entity(name='disp__completedcard', value_type=ValueType.STRING, join_key='disp_id')
 
 district__completeddistrict = Entity(name='district__completeddistrict', value_type=ValueType.INT64, join_key='district_id')
 
-order__completedorder = Entity(name='order__completedorder', value_type=ValueType.INT64, join_key='order_id')
+loan__luxuryloanportfolio = Entity(name='loan__luxuryloanportfolio', value_type=ValueType.STRING, join_key='loan_id')
 
-client__completedclient = Entity(name='client__completedclient', value_type=ValueType.STRING, join_key='client_id')
+order__completedorder = Entity(name='order__completedorder', value_type=ValueType.INT64, join_key='order_id')
 
 account__month_summary = Entity(name='account__month_summary', value_type=ValueType.STRING, join_key='account_id')
 
 client__month_summary = Entity(name='client__month_summary', value_type=ValueType.INT64, join_key='client_id')
 
-Feature_view_1 = FeatureView(
-name='Feature_view_1',
-entities=['loan__luxuryloanportfolio'],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/luxuryloanportfolio.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_10 = FeatureView(
-name='Feature_view_10',
-entities=['card__completedcard'],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedcard.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_13 = FeatureView(
-name='Feature_view_13',
-entities=['account__month_summary', 'client__month_summary'],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/month_summary.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_2 = FeatureView(
-name='Feature_view_2',
-entities=['account__completedloan'],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedloan.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_3 = FeatureView(
-name='Feature_view_3',
+Feature_view_7 = FeatureView(
+name='Feature_view_7',
 entities=['account__completedacct'],
 ttl=timedelta(days=30),
 online=True,
@@ -79,18 +41,8 @@ path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data
 event_timestamp_column='event_timestamp')
 )
 
-Feature_view_4 = FeatureView(
-name='Feature_view_4',
-entities=['client__completedclient'],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedclient.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_6 = FeatureView(
-name='Feature_view_6',
+Feature_view_9 = FeatureView(
+name='Feature_view_9',
 entities=['district__completeddistrict'],
 ttl=timedelta(days=30),
 online=True,
@@ -99,13 +51,83 @@ path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data
 event_timestamp_column='event_timestamp')
 )
 
-Feature_view_7 = FeatureView(
-name='Feature_view_7',
+Feature_view_13 = FeatureView(
+name='Feature_view_13',
+entities=['disp__completedcard'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedcard.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_11 = FeatureView(
+name='Feature_view_11',
+entities=['account__month_summary', 'client__month_summary'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/month_summary.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_5 = FeatureView(
+name='Feature_view_5',
+entities=['account__completedloan'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedloan.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_10 = FeatureView(
+name='Feature_view_10',
+entities=['client__completeddisposition'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completeddisposition.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_8 = FeatureView(
+name='Feature_view_8',
 entities=['order__completedorder'],
 ttl=timedelta(days=30),
 online=True,
 batch_source=FileSource(
 path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedorder.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_1 = FeatureView(
+name='Feature_view_1',
+entities=['client__completedclient'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/completedclient.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_3 = FeatureView(
+name='Feature_view_3',
+entities=['loan__luxuryloanportfolio'],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/luxuryloanportfolio.parquet',
+event_timestamp_column='event_timestamp')
+)
+
+Feature_view_14 = FeatureView(
+name='Feature_view_14',
+entities=[],
+ttl=timedelta(days=30),
+online=True,
+batch_source=FileSource(
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/district.parquet',
 event_timestamp_column='event_timestamp')
 )
 
@@ -119,32 +141,12 @@ path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data
 event_timestamp_column='event_timestamp')
 )
 
-Feature_view_11 = FeatureView(
-name='Feature_view_11',
+Feature_view_6 = FeatureView(
+name='Feature_view_6',
 entities=[],
 ttl=timedelta(days=30),
 online=True,
 batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/district.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_5 = FeatureView(
-name='Feature_view_5',
-entities=[],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/crm_call_center_logs.parquet',
-event_timestamp_column='event_timestamp')
-)
-
-Feature_view_14 = FeatureView(
-name='Feature_view_14',
-entities=[],
-ttl=timedelta(days=30),
-online=True,
-batch_source=FileSource(
-path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/client.parquet',
+path=r'/Users/shubhamvashisth/Documents/CoDS/projects/KGFarm/helpers/sample_data/parquet/retail-bankingdemodata/crm_reviews.parquet',
 event_timestamp_column='event_timestamp')
 )
