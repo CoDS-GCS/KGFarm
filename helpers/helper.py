1892f8b391fb4bba7ab0256913af9b03db1fd2c6
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-07-20 16:21:58 -0400

    add library prefix

--- helpers/helper.py
+++ helpers/helper.py
@@ -16,6 +16,7 @@ PREFIXES = """
     PREFIX rdf:         <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs:        <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX pipeline:    <http://kglids.org/ontology/pipeline/>
+    PREFIX lib:         <http://kglids.org/resource/library/> 
     """
 
 
