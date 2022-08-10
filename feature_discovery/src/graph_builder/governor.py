8c40137bd9a67a59ecc57c862a5afb10d883bb74
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-08-09 23:41:19 -0400

    add support for updating physical representation of an entity on the fly directly to the KG

--- feature_discovery/src/graph_builder/governor.py
+++ feature_discovery/src/graph_builder/governor.py
@@ -1,5 +1,4 @@
-import pandas as pd
-
+from tqdm import tqdm
 from operations.template import *
 
 
@@ -8,7 +7,7 @@ class Governor:
         self.config = config
 
     def drop_feature_view(self, drop: list):
-        for feature_view_info in drop:
+        for feature_view_info in tqdm(drop):
             feature_view = feature_view_info['Feature_view']
             if exists(self.config, feature_view):
                 drop_feature_view(self.config, feature_view)
@@ -17,5 +16,15 @@ class Governor:
                 raise ValueError(error)
         print('Dropped ', [i['Feature_view'] for i in drop], 'successfully!')
 
+    # TODO: investigate why after updating default entity, kgfarm.get_optional_representations() doesn't returns the removed default entity
     def update_entity(self, entity_updation_info: list):
-        print('Governor under construction!')
+        for updation_info in tqdm(entity_updation_info):
+            feature_view = updation_info['Feature_view']
+            entity = updation_info['Entity']
+            column_physical_representation_to_be_inserted = updation_info['Optional_physical_representation']
+            remove_current_physical_representation_of_an_entity(self.config, feature_view=feature_view)
+            insert_current_physical_representation_of_an_entity(self.config, feature_view=feature_view,
+                                   column=column_physical_representation_to_be_inserted, entity=entity)
+            print('Entity {} uses {} as its physical representation'.
+                  format(entity, column_physical_representation_to_be_inserted))
+
