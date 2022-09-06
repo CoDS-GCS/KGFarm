b9fbbb8a4008f298834aad68928b7abfea25d5e4
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-09-06 16:40:55 -0400

    add cross validation and hyperparameter optimization to transformation recommender

--- feature_discovery/src/recommender/build.py
+++ feature_discovery/src/recommender/build.py
@@ -4,11 +4,12 @@ import warnings
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+
 warnings.filterwarnings('ignore')
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import precision_score, recall_score, f1_score,  classification_report
+from sklearn.metrics import classification_report, make_scorer
 from helpers.helper import connect_to_stardog
 from operations.template import get_transformations_on_columns
 
@@ -68,13 +69,13 @@ class Recommender:
         # filter dataset based on feature data-type
         transformations_on_columns = transformations_on_columns. \
             loc[transformations_on_columns['Data_type'] == self.feature_type]
-        print('{} {} samples found'.format(len(transformations_on_columns), self.feature_type))
+        print('{} {} data points found'.format(len(transformations_on_columns), self.feature_type))
 
         self.modeling_data = transformations_on_columns
 
         transformations_on_columns.to_csv('data.csv', index=False)
 
-    def eda(self):
+    def eda(self, plot: bool = True):
 
         def clean():
             self.modeling_data.drop(['Column_id', 'Data_type'], axis=1, inplace=True)
@@ -111,54 +112,62 @@ class Recommender:
 
         clean()
         self.transformations = set(list(self.modeling_data['Transformation']))
-        plot_class_distribution()
+        if plot:
+            plot_class_distribution()
         transform()
 
         self.modeling_data.to_csv('data.csv', index=False)
 
     def define(self):
         self.classifier = RandomForestClassifier()
+        hyperparameters = {'n_estimators': [100, 200, 300]}
+        return hyperparameters
 
-    def fit(self):
+    def train_test_evaluate(self, parameters: dict):
+
+        def optimize():
+            self.classifier = GridSearchCV(estimator=self.classifier, param_grid=parameters, cv=inner_cv)
+
+        def evaluate():
+            y_true = []
+            y_pred = []
+
+            def score(y_true_label, y_pred_label):
+                y_true.extend(y_true_label)
+                y_pred.extend(y_pred_label)
+
+            cross_val_score(self.classifier, X, y, cv=outer_cv, scoring=make_scorer(score))
+            return y_true, y_pred
+
         X = list(self.modeling_data['Embeddings'])
         y = self.modeling_data['Transformation']
 
-        # TODO: use cross-validation
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
-        self.classifier.fit(X_train, y_train)
-        return X_test, y_test
+        # Nested CV with parameter optimization
+        inner_cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+        outer_cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
 
-    def evaluate(self, testing_data: tuple):
-        X_test = testing_data[0]
-        y_test = testing_data[1]
-        accuracy = self.classifier.score(X_test, y_test)
-        y_pred = self.classifier.predict(X_test)
+        optimize()  # Hyperparameter optimization (using nested CV to prevent leakage)
+        true, pred = evaluate()
+
+        # Average scores over all folds
+        return classification_report(y_true=true, y_pred=pred,
+                                     labels=self.encoder.transform(list(self.transformations)),
+                                     target_names=list(self.transformations))
 
         # TODO: plot all metrics for both feature types
-        precision = precision_score(y_true=y_test, y_pred=y_pred, average='macro')
-        recall = recall_score(y_true=y_test, y_pred=y_pred, average='macro')
-        f1 = f1_score(y_true=y_test, y_pred=y_pred, average='macro')
-        print(classification_report(y_true=y_test, y_pred=y_pred,
-                                    labels=self.encoder.transform(list(self.transformations)),
-                                    target_names=list(self.transformations)))
-        print('Accuracy: ', accuracy)
-        print('Precision: ', precision)
-        print('Recall: ', recall)
-        print('F1:  ', f1)
 
-        return accuracy, precision, recall, f1
-
-    def save(self, scores: tuple):
+    @staticmethod
+    def save(scores: str):
+        print(scores)
         pass
 
 
 def build():
-    for feature_type in ['numeric', 'string']:
+    for feature_type in ['string', 'numeric']:
         recommender = Recommender(feature_type=feature_type)
         recommender.generate_modelling_data()
-        recommender.eda()
-        recommender.define()
-        recommender.save(scores=recommender.evaluate(testing_data=recommender.fit()))
+        recommender.eda(plot=False)
+        recommender.save(scores=recommender.train_test_evaluate(parameters=recommender.define()))
     print('done.')
 
 
