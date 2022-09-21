fe366ba35b1442078902aee3ce6fae7b92487ddc
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2022-09-21 15:51:32 -0400

    handle feature leakage for feature scaling and normalization

---
+++ operations/recommendation/utils/transformation_mappings.py
@@ -0,0 +1,6 @@
+transformation_mapping = {
+                        'MinMaxScaler': 'Feature normalization',
+                        'OneHotEncoder': 'Categorical feature encoding',
+                        'LabelEncoder': 'Categorical feature encoding',
+                        'StandardScaler': 'Feature scaling',
+                        'RobustScaler': 'Feature scaling'}
