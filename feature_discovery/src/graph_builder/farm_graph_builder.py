b55c455ced8e483999111587e7228234be45cc30
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2023-01-25 16:19:56 -0500

    generate farm graph via governor

---
+++ feature_discovery/src/graph_builder/farm_graph_builder.py
@@ -0,0 +1,3 @@
+def build_farm_graph(config, graph_name: str):
+    print('Hi')
+
--- feature_discovery/src/graph_builder/governor.py
+++ feature_discovery/src/graph_builder/governor.py
@@ -1,11 +1,17 @@
 from tqdm import tqdm
-
 from operations.template import *
+from helpers.helper import connect_to_stardog
+from feature_discovery.src.graph_builder.farm_graph_builder import build_farm_graph
 
 
 class Governor:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, config=None, graph_name: str = 'farm.ttl', lids_db: str = 'kgfarm_test', port: int = 5820,
+                 show_connection_status: bool = False):
+        if config is None:
+            self.config = connect_to_stardog(port=port, db=lids_db, show_status=show_connection_status)
+        else:
+            self.config = config
+        self.graph_name = graph_name
 
     def drop_feature_view(self, drop: list):
         for feature_view_info in tqdm(drop):
@@ -24,7 +30,15 @@ class Governor:
             column_physical_representation_to_be_inserted = updation_info['Optional_physical_representation']
             remove_current_physical_representation_of_an_entity(self.config, feature_view=feature_view)
             insert_current_physical_representation_of_an_entity(self.config, feature_view=feature_view,
-                                   column=column_physical_representation_to_be_inserted, entity=entity)
+                                                                column=column_physical_representation_to_be_inserted,
+                                                                entity=entity)
             print("Entity '{}' uses '{}' as its physical representation".
                   format(entity, column_physical_representation_to_be_inserted))
 
+    def build_farm_graph(self):
+        build_farm_graph(config=self.config, graph_name=self.graph_name)
+
+
+if __name__ == "__main__":
+    governor = Governor()
+    governor.build_farm_graph()
