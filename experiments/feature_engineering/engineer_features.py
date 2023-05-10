bug fixes in recommender: handle datasets with no numerical features

experiments/feature_engineering/engineer_features.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import time
+import dask
 import psutil
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
 from sklearn.preprocessing import *
+from dask.dataframe import from_pandas
 from sklearn.linear_model import ElasticNet
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.metrics import f1_score, r2_score, mean_squared_error
-from sklearn.feature_selection import mutual_info_classif, f_classif, SelectKBest, f_regression
+from sklearn.feature_selection import mutual_info_classif, SelectKBest, f_regression, f_classif
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, GradientBoostingRegressor
 
 spath = str(Path(os.getcwd()).resolve().parents[1])
@@ -27,9 +29,10 @@ np.random.seed(RANDOM_STATE)
 
 
 class EngineerFeatures:
-    def __init__(self, path: str, theta1: float, theta2: float):
-        self.theta1 = theta1
-        self.theta2 = theta2
+    def __init__(self, path: str, information_gain_thresh: float, correlation_thresh: float, feature_selection_thresh: float):
+        self.theta1 = information_gain_thresh
+        self.theta2 = correlation_thresh
+        self.theta3 = feature_selection_thresh
         self.path_to_dataset = path
         self.experiment_datasets_info = pd.read_csv('automl_datasets.csv')
         self.working_dir = os.getcwd()
@@ -63,6 +66,7 @@ class EngineerFeatures:
         else:
             X, y = self.__separate_independent_and_dependent_variables(df=train_set, target=target)
             X = X[numerical_features]  # consider numerical features for pruning
+            X_parallelized = dask.dataframe.from_pandas(X, npartitions=os.cpu_count()-1).values
 
             if task == 'regression':
                 feature_pruning_model = SelectKBest(f_regression, k='all')
@@ -71,9 +75,8 @@ class EngineerFeatures:
             else:
                 feature_pruning_model = SelectKBest(mutual_info_classif, k='all')
 
-            feature_pruning_model.fit(X=X, y=y)
-
-            for feature, score in zip(X.columns, feature_pruning_model.scores_):
+            feature_pruning_model.fit(X=X_parallelized, y=y)
+            for feature, score in zip(X.columns, dask.compute(feature_pruning_model.scores_)[0]):
                 if score > self.theta1:
                     self.pruning_info[feature] = score
 
@@ -81,8 +84,8 @@ class EngineerFeatures:
                                  sorted(self.pruning_info.items(), key=lambda item: item[1], reverse=True)}
 
             if (df_size >= 5 and len(numerical_features) >= 100) or (df_size >= 20):
-                pruned_features = list(self.pruning_info.keys())[:10]
-            elif len(self.pruning_info) == 0:
+                pruned_features = list(self.pruning_info.keys())[:20]
+            elif len(self.pruning_info) < int(0.1*len(X.columns)) or len(self.pruning_info) <= 1:
                 pruned_features = list(X.columns)
             else:
                 pruned_features = list(self.pruning_info.keys())
@@ -100,8 +103,7 @@ class EngineerFeatures:
         1. compute pairwise correlation
         2. drop features with the lowest feature importance
         """
-        features = train_set.drop(target, axis=1).corr()
-
+        features = from_pandas(data=train_set.drop(target, axis=1), npartitions=os.cpu_count()-1).corr().compute()
         features_to_discard = set()
         for feature_i, i in zip(features.columns, range(len(features))):
             for j in range(i):
@@ -114,7 +116,7 @@ class EngineerFeatures:
         features_filtered_by_correlation = [feature for feature in features.columns if
                                             feature not in features_to_discard]
 
-        if len(features_filtered_by_correlation) == 0:
+        if len(features_filtered_by_correlation) < int(0.1*len(features)) or len(features_filtered_by_correlation) <= 1:
             features_filtered_by_correlation = list(train_set.columns)
         else:
             features_filtered_by_correlation.append(target)
@@ -122,22 +124,6 @@ class EngineerFeatures:
 
     def __transform(self, train_set: pd.DataFrame, test_set: pd.DataFrame, target: str):
         X, _ = self.__separate_independent_and_dependent_variables(df=train_set, target=target)
-        """
-        recommended_transformations = self.kgfarm.recommend_data_transformations(entity_df=X, show_query=False, show_insights=False)
-
-        if isinstance(recommended_transformations, type(None)):
-            print('data does not requires transformation')
-            return train_set, test_set
-
-        else:
-            for n, recommendation in recommended_transformations.to_dict('index').items():
-                train_set, _ = self.kgfarm.apply_transformation(transformation_info=recommended_transformations.iloc[n],
-                                                                entity_df=train_set, output_message='min')
-                test_set, _ = self.kgfarm.apply_transformation(transformation_info=recommended_transformations.iloc[n],
-                                                               entity_df=test_set, output_message='min')
-
-            return train_set, test_set
-        """
         recommended_transformations = self.kgfarm.recommend_transformations(X=X)  # recommendations on train-set
         if isinstance(recommended_transformations, type(None)):
             print('data does not requires transformation')
@@ -155,13 +141,23 @@ class EngineerFeatures:
                 for i, transformation_info in unary_categorical_transformations.to_dict('index').items():
                     transformation = transformation_info['Recommended_transformation']
                     features_to_transform = transformation_info['Feature']
+                    print(f'Applying {transformation} on {features_to_transform}')
                     if transformation == 'OrdinalEncoder':
-                        train_set, encoder = self.kgfarm.apply_transformations(X=train_set, recommendation=
-                        unary_numerical_transformations.iloc[i])
+                        encoder = OrdinalEncoder()
+                        encoder.fit(X=pd.concat([train_set[features_to_transform], test_set[features_to_transform]]))
+                        train_set[features_to_transform] = encoder.transform(train_set[features_to_transform])
                         test_set[features_to_transform] = encoder.transform(test_set[features_to_transform])
                     else:
-                        train_set, encoder = self.kgfarm.apply_transformations(X=train_set, recommendation=
-                        unary_numerical_transformations.iloc[i])
+                        encoder = OneHotEncoder(handle_unknown='ignore')
+                        encoder.fit(X=pd.concat([train_set[features_to_transform], test_set[features_to_transform]]))
+                        one_hot_encoded_features_train_set = pd.DataFrame(encoder.transform(train_set[features_to_transform]).toarray())
+                        train_set = train_set.join(one_hot_encoded_features_train_set)
+                        train_set = train_set.drop(features_to_transform, axis=1)
+                        one_hot_encoded_features_test_set = pd.DataFrame(encoder.transform(test_set[features_to_transform]).toarray())
+                        test_set = test_set.join(one_hot_encoded_features_test_set)
+                        test_set = test_set.drop(one_hot_encoded_features_test_set, axis=1)
+
+                        train_set, encoder = self.kgfarm.apply_transformations(X=train_set[features_to_transform], recommendation=unary_categorical_transformations.iloc[i])
                         test_set[features_to_transform] = encoder.transform(test_set[features_to_transform])
                         one_hot_encoded_features = pd.DataFrame(
                             encoder.transform(test_set[features_to_transform]).toarray())
@@ -194,9 +190,8 @@ class EngineerFeatures:
                 for i, transformation_info in unary_numerical_transformations.to_dict('index').items():
                     transformation = transformation_info['Recommended_transformation']
                     features_to_transform = transformation_info['Feature']
+                    print(f'Applying {transformation} to {features_to_transform}')
                     if transformation == 'Log':
-                        print(f'Applying {transformation} to {features_to_transform}')
-
                         def log_plus_const(x, const=0):
                             return np.log(x + np.abs(const) + 0.0001)
 
@@ -217,8 +212,6 @@ class EngineerFeatures:
                                 X=np.array(test_set[f]).reshape(-1, 1))
 
                     elif transformation == 'Sqrt':
-                        print(f'Applying {transformation} to {features_to_transform}')
-
                         def sqrt_plus_const(x, const=0):
                             return np.sqrt(x + np.abs(const) + 0.0001)
 
@@ -245,31 +238,36 @@ class EngineerFeatures:
                             test_set[features_to_transform])
             return train_set, test_set
 
-    def __select_features(self, train_set: pd.DataFrame, test_set: pd.DataFrame, target: str, task: str):
+    def __select_features(self, train_set: pd.DataFrame, test_set: pd.DataFrame, target: str):
         X, y = self.__separate_independent_and_dependent_variables(df=train_set, target=target)
         filter_model = SelectKBest(f_classif, k='all')
         filter_model.fit(X=X, y=y)
 
-        features_filtered_by_anova = set()
+        features_filtered = set()
         for feature, score in zip(X.columns, filter_model.scores_):
-            if score > 1.00:
-                features_filtered_by_anova.add(feature)
+            if score > self.theta3:
+                features_filtered.add(feature)
 
-        features_filtered_by_anova.add(target)
+        if len(features_filtered) < int(0.1*len(X.columns)) or len(features_filtered) <= 1:
+            features_filtered = set(X.columns)
+
+        features_filtered.add(target)
+        """
         try:
             feature_selection_recommendation = self.kgfarm.recommend_features_to_be_selected(
-                entity_df=train_set[features_filtered_by_anova], dependent_variable=target, task=task, n=300)
+                entity_df=train_set[features_filtered], dependent_variable=target, task=task, n=300)
             feature_selection_recommendation = feature_selection_recommendation.loc[
                 feature_selection_recommendation['Selection_score'] > 0.60]
         except AttributeError:
-            return train_set[features_filtered_by_anova], test_set[features_filtered_by_anova]
-
+            return train_set[features_filtered], test_set[features_filtered]
         if len(feature_selection_recommendation) == 0:
             features_filtered_by_kgfarm = list(train_set.columns)
         else:
             features_filtered_by_kgfarm = feature_selection_recommendation['Feature'].tolist()
             features_filtered_by_kgfarm.append(target)
         return train_set[features_filtered_by_kgfarm], test_set[features_filtered_by_kgfarm]
+        """
+        return train_set[features_filtered], test_set[features_filtered]
 
     def __train_and_evaluate(self, train_set: pd.DataFrame, test_set: pd.DataFrame, target: str, task: str):
         X_train, y_train = self.__separate_independent_and_dependent_variables(df=train_set, target=target)
@@ -313,9 +311,9 @@ class EngineerFeatures:
                 result_f1_r2 = {'KNN': 0, 'RF': 0, 'NN': 0}
                 result_acc_mse = {'KNN': 0, 'RF': 0, 'NN': 0}
                 df[target] = LabelEncoder().fit_transform(df[target])
-                self.models = [KNeighborsClassifier(weights='distance'),
+                self.models = [KNeighborsClassifier(),
                                RandomForestClassifier(class_weight='balanced', random_state=RANDOM_STATE),
-                               MLPClassifier(hidden_layer_sizes=(50, 25), random_state=RANDOM_STATE)]
+                               MLPClassifier(random_state=RANDOM_STATE)]
                 folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=RANDOM_STATE)
 
             else:
@@ -340,34 +338,34 @@ class EngineerFeatures:
                 train_set = df.iloc[train_index]
                 test_set = df.iloc[test_index]
 
-                print(f'computing feature importance for {n_features} features')
+                print(f'computing feature importance for {n_features} features', end=' ')
+
                 train_set, test_set = self.__compute_feature_importance(train_set=train_set, test_set=test_set,
                                                                         target=target, df_size=df_size,
                                                                         task=dataset_info['Task'],
                                                                         categorical_features=categorical_features,
                                                                         numerical_features=numerical_features)
-                print(f'information gain done, # features: {len(train_set.columns) - 1}')
+                print(f'done. | # features: {len(train_set.columns) - 1}')
 
-                print(f'computing feature correlation b/w {len(train_set.columns) - 1} features')
+                print(f'computing feature correlation b/w {len(train_set.columns) - 1} features', end=' ')
                 train_set, test_set = self.__compute_feature_correlation(train_set=train_set, test_set=test_set,
                                                                          target=target)
-                print(f'correlation done, # features: {len(train_set.columns) - 1}')
+                print(f'done. | # features: {len(train_set.columns) - 1}')
 
-                print(f'recommending and applying transformation on {len(train_set.columns) - 1} features')
+                print(f'recommending data transformations on {len(train_set.columns) - 1} features', end=' ')
                 train_set, test_set = self.__transform(train_set=train_set, test_set=test_set, target=target)
-                print(f'transformations done, # features: {len(train_set.columns) - 1}')
+                print(f'done.')
 
-                print(f'computing feature importance of {len(train_set.columns) - 1} features')
-                train_set, test_set = self.__select_features(train_set=train_set, test_set=test_set, target=target,
-                                                             task=dataset_info['Task'])
-                print(f'feature selection done, # features: {len(train_set.columns) - 1}')
+                print(f'computing feature importance for {len(train_set.columns) - 1} features', end=' ')
+                train_set, test_set = self.__select_features(train_set=train_set, test_set=test_set, target=target)
+                print(f'done. | # features: {len(train_set.columns) - 1}')
 
                 print(f'training and evaluating models on {len(train_set.columns) - 1} features')
                 f1_r2, acc_mse = self.__train_and_evaluate(train_set=train_set, test_set=test_set, target=target,
                                                            task=dataset_info['Task'])
                 f1_r2_per_dataset.append(f1_r2)
                 acc_mse_per_dataset.append(acc_mse)
-                print('model training and evaluation done.')
+                print('done.')
 
                 print(f'{dataset_info["Dataset"]} fold-{fold} completed.\n', '-' * 100)
                 fold = fold + 1
@@ -408,5 +406,5 @@ class EngineerFeatures:
 
 
 if __name__ == '__main__':
-    experiment = EngineerFeatures(path='../../../../../data/automl_datasets/', theta1=0.00, theta2=0.90)
+    experiment = EngineerFeatures(path='../../../../../data/automl_datasets/', information_gain_thresh=0.00, correlation_thresh=0.90, feature_selection_thresh=1.00)
     experiment.run()

operations/recommendation/recommender.py
@@ -232,27 +232,31 @@ class Recommender:
         numerical_feature_embeddings, categorical_feature_embeddings = self.compute_content_embedding_parallel(
             entity_df=X)
 
-        # scaling transformation
-        recommended_scaler_transformation = list(self.scaler_encoder.inverse_transform(self.scaler_model. \
-            predict(np.array(average_embeddings(embeddings=list(numerical_feature_embeddings.values()))).reshape(1, -1))))[0]
-        recommendations.append(pd.DataFrame({'Feature': [list(X.columns)], 'Recommended_transformation': recommended_scaler_transformation}))
-        # unary transformation (numeric features)
-        numeric_embedding_df = pd.DataFrame(
-            {'Feature': numerical_feature_embeddings.keys(), 'Embedding': numerical_feature_embeddings.values()})
-        numeric_embedding_df['Probability'] = self.unary_model.predict_proba(list(numeric_embedding_df['Embedding'])).tolist()
-        numeric_embedding_df['Transform'] = numeric_embedding_df['Probability'].apply(
-            lambda x: True if self.unary_transformation_threshold <= max(x) else False)
-        numeric_embedding_df = numeric_embedding_df.loc[numeric_embedding_df['Transform'] == True]
-        if numeric_embedding_df.empty:
-            recommendations.append(pd.DataFrame(columns=['Feature', 'Recommended_transformation']))
-        else:
-            numeric_embedding_df['Recommended_transformation'] = self.unary_encoder.inverse_transform(
-                self.unary_model.predict(list(numeric_embedding_df['Embedding']))).tolist()
-            unary_numeric_transformation_df = numeric_embedding_df[['Feature', 'Recommended_transformation']].groupby('Recommended_transformation')['Feature'].apply(list).to_frame()
-            unary_numeric_transformation_df['Recommended_transformation'] = list(unary_numeric_transformation_df.index)
-            unary_numeric_transformation_df = unary_numeric_transformation_df.reset_index(drop=True)
-            recommendations.append(unary_numeric_transformation_df)
+        try:
+            # scaling transformation
+            recommended_scaler_transformation = list(self.scaler_encoder.inverse_transform(self.scaler_model. \
+                predict(np.array(average_embeddings(embeddings=list(numerical_feature_embeddings.values()))).reshape(1, -1))))[0]
+            recommendations.append(pd.DataFrame({'Feature': [list(X.columns)], 'Recommended_transformation': recommended_scaler_transformation}))
 
+            # unary transformation (numeric features)
+            numeric_embedding_df = pd.DataFrame(
+                {'Feature': numerical_feature_embeddings.keys(), 'Embedding': numerical_feature_embeddings.values()})
+            numeric_embedding_df['Probability'] = self.unary_model.predict_proba(list(numeric_embedding_df['Embedding'])).tolist()
+            numeric_embedding_df['Transform'] = numeric_embedding_df['Probability'].apply(
+                lambda x: True if self.unary_transformation_threshold <= max(x) else False)
+            numeric_embedding_df = numeric_embedding_df.loc[numeric_embedding_df['Transform'] == True]
+            if numeric_embedding_df.empty:
+                recommendations.append(pd.DataFrame(columns=['Feature', 'Recommended_transformation']))
+            else:
+                numeric_embedding_df['Recommended_transformation'] = self.unary_encoder.inverse_transform(
+                    self.unary_model.predict(list(numeric_embedding_df['Embedding']))).tolist()
+                unary_numeric_transformation_df = numeric_embedding_df[['Feature', 'Recommended_transformation']].groupby('Recommended_transformation')['Feature'].apply(list).to_frame()
+                unary_numeric_transformation_df['Recommended_transformation'] = list(unary_numeric_transformation_df.index)
+                unary_numeric_transformation_df = unary_numeric_transformation_df.reset_index(drop=True)
+                recommendations.append(unary_numeric_transformation_df)
+        except IndexError:
+            recommendations.append(
+                pd.DataFrame({'Feature': [list(X.columns)], 'Recommended_transformation': 'StandardScaler'}))
         # unary transformation (categorical features)
         if categorical_feature_embeddings:
             categorical_embedding_df = pd.DataFrame(