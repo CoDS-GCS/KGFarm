4d8a3e6ebfc44fa52ee4c8cd879ae50eb7fafcbe
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2023-07-06 01:52:01 -0400

    add feature engineering api to interface

--- interface/apis.py
+++ interface/apis.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import *
 from matplotlib import pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.feature_selection import mutual_info_classif, SelectKBest, f_regression
 from inference_manager.inference import InferenceManager
 
 warnings.filterwarnings('ignore')
@@ -114,6 +115,27 @@ class KGFarm:
         columns.reset_index(drop=True, inplace=True)
         return columns
 
+    @staticmethod
+    def determine_ml_task(y: pd.Series):
+        y_cardinality = len(y.unique())
+        if y_cardinality == 2:
+            return 'binary'
+        elif y_cardinality > 2 and y_cardinality/len(y) < 0.5:
+            return 'multiclass'
+        else:
+            return 'regression'
+
+    @staticmethod
+    def get_feature_dtypes(features: pd.DataFrame):
+        feature_dtype_info = pd.DataFrame(features.dtypes)
+        feature_dtype_info['Feature'] = feature_dtype_info.index
+        feature_dtype_info = feature_dtype_info.rename(columns={0: 'Type'}).reset_index(drop=True)
+        feature_dtype_info['Type'] = feature_dtype_info['Type'].apply(
+            lambda x: 'categorical' if x == 'object' else 'numerical')
+        numerical_features = list(feature_dtype_info.loc[feature_dtype_info['Type'] == 'numerical']['Feature'])
+        categorical_features = list(feature_dtype_info.loc[feature_dtype_info['Type'] == 'categorical']['Feature'])
+        return numerical_features, categorical_features
+
     def recommend_cleaning_operations(self, df: pd.DataFrame, visualize_missing_data: bool = True):
 
         def plot_heat_map():
@@ -212,3 +234,191 @@ class KGFarm:
 
         return self.recommender.get_feature_selection_score(task=task, entity_df=entity_df.sample(n=k, random_state=1),
                                                             dependent_variable=str(y.name))
+
+    def engineer_features(self, train_set: pd.DataFrame, test_set: pd.DataFrame, target: str, information_gain_thresh: float = 0.00, correlation_thresh: float = 0.90):
+        pruning_info = {}
+        df_size = train_set.memory_usage(deep=True).sum() / (1024 * 1024)
+        task = self.determine_ml_task(y=pd.concat([train_set[target], test_set[target]]))
+        numerical_features, categorical_features = self.get_feature_dtypes(features=train_set)
+
+        def compute_feature_importance(train: pd.DataFrame, test: pd.DataFrame, theta1: float):
+            nonlocal pruning_info
+            X = train[numerical_features]
+            if target in list(X.columns):
+                X = X.drop(target, axis=1)
+            y = train[target]
+
+            if task == 'binary' or task == 'multiclass':
+                feature_pruning_model = SelectKBest(mutual_info_classif, k='all')
+            else:
+                feature_pruning_model = SelectKBest(f_regression, k='all')
+                theta1 = theta1 + 5
+
+            feature_pruning_model.fit(X=X, y=y)
+
+            for feature, score in zip(X.columns, feature_pruning_model.scores_):
+                if score > theta1:
+                    pruning_info[feature] = score
+
+            pruning_info = {k: v for k, v in
+                                 sorted(pruning_info.items(), key=lambda item: item[1], reverse=True)}
+
+            if (df_size >= 5 and len(numerical_features) >= 100) or (df_size >= 20):
+                pruned_features = list(pruning_info.keys())[:20]
+            elif len(pruning_info) < int(0.1 * len(X.columns)) or len(pruning_info) <= 1:
+                pruned_features = list(X.columns)
+            else:
+                pruned_features = list(pruning_info.keys())
+
+            if target not in pruned_features:
+                pruned_features.append(target)
+
+            # TODO: fix this hack for categorical features
+            if len(categorical_features) > 0:
+                pruned_features.extend([c for c in categorical_features if c.lower() != 'name'])
+            return train[pruned_features], test[pruned_features]
+
+        def compute_feature_correlation(train: pd.DataFrame, test: pd.DataFrame, theta2: float):
+            nonlocal pruning_info
+            features = train[set(numerical_features).intersection(set(pruning_info.keys()))]
+            if target in list(features.columns):
+                features = train.drop(target, axis=1)
+            features_to_discard = set()
+            for feature_i, i in zip(features.columns, range(len(features))):
+                for j in range(i):
+                    if features.iloc[i, j] > theta2:
+                        if pruning_info.get(features.columns[j]) > pruning_info.get(feature_i):
+                            features_to_discard.add(feature_i)
+                        else:
+                            features_to_discard.add(features.columns[j])
+
+            features_filtered_by_correlation = [feature for feature in features.columns if
+                                                feature not in features_to_discard]
+
+            if len(features_filtered_by_correlation) < int(0.1 * len(features)) or len(
+                    features_filtered_by_correlation) <= 1:
+                features_filtered_by_correlation = list(train.columns)
+            else:
+                features_filtered_by_correlation.append(target)
+
+            return train[features_filtered_by_correlation], test[features_filtered_by_correlation]
+
+        def apply_transform_fe(train: pd.DataFrame, test: pd.DataFrame):
+            X = train.drop(target, axis=1)
+            recommended_transformations = self.recommend_transformations(X=X)  # recommendations on train-set
+            if isinstance(recommended_transformations, type(None)):
+                print('data does not requires transformation')
+                return train, test
+
+            unary_categorical_transformations = recommended_transformations.loc[
+                recommended_transformations['Transformation_type'] == 'categorical']
+            unary_numerical_transformations = recommended_transformations.loc[
+                recommended_transformations['Transformation_type'] == 'unary']
+            scaling_transformation = recommended_transformations.loc[
+                recommended_transformations['Transformation_type'] == 'scaling']
+
+            if not unary_categorical_transformations.empty:
+                print('transforming categorical features')
+                for i, transformation_info in unary_categorical_transformations.to_dict('index').items():
+                    transformation = transformation_info['Recommended_transformation']
+                    features_to_transform = transformation_info['Feature']
+                    print(f'Applying {transformation} on {features_to_transform}')
+                    if transformation == 'OrdinalEncoder':
+                        encoder = OrdinalEncoder()
+                        encoder.fit(
+                            X=pd.concat([train[features_to_transform], test[features_to_transform]]))
+                        train[features_to_transform] = encoder.transform(train[features_to_transform])
+                        test[features_to_transform] = encoder.transform(test[features_to_transform])
+                    else:
+                        encoder = OneHotEncoder(handle_unknown='ignore')
+                        train.reset_index(drop=True, inplace=True)
+                        one_hot_encoded_features = pd.DataFrame(
+                            encoder.fit_transform(train[features_to_transform]).toarray())
+                        train = pd.concat([train, one_hot_encoded_features], axis=1)
+                        train = train.drop(features_to_transform, axis=1)
+                        train.columns = train.columns.astype(str)
+                        test.reset_index(drop=True, inplace=True)
+                        one_hot_encoded_features = pd.DataFrame(
+                            encoder.fit_transform(test[features_to_transform]).toarray())
+                        test = pd.concat([test, one_hot_encoded_features], axis=1)
+                        test = test.drop(features_to_transform, axis=1)
+                        test.columns = test.columns.astype(str)
+
+            if not scaling_transformation.empty:
+                print('scaling features')
+                for i, transformation_info in scaling_transformation.to_dict('index').items():
+                    transformation = transformation_info['Recommended_transformation']
+                    features_to_transform = list(train.drop(target, axis=1).columns)
+                    if transformation in {'StandardScaler', 'MinMaxScaler', 'RobustScaler', 'QuantileTransformer',
+                                          'PowerTransformer'}:
+                        print(f'Applying {transformation} on {features_to_transform}')
+                        if transformation == 'StandardScaler':
+                            scaler = StandardScaler()
+                        elif transformation == 'MinMaxScaler':
+                            scaler = MinMaxScaler()
+                        elif transformation == 'RobustScaler':
+                            scaler = RobustScaler()
+                        elif transformation == 'QuantileTransformer':
+                            scaler = QuantileTransformer()
+                        else:
+                            scaler = PowerTransformer()
+                        train[features_to_transform] = scaler.fit_transform(X=train[features_to_transform])
+                        test[features_to_transform] = scaler.transform(X=test[features_to_transform])
+
+            if not unary_numerical_transformations.empty:
+                print('transforming numerical features using unary transformations')
+                for i, transformation_info in unary_numerical_transformations.to_dict('index').items():
+                    transformation = transformation_info['Recommended_transformation']
+                    features_to_transform = transformation_info['Feature']
+                    print(f'Applying {transformation} to {features_to_transform}')
+                    if transformation == 'Log':
+                        def log_plus_const(x, const=0):
+                            return np.log(x + np.abs(const) + 0.0001)
+
+                        for f in tqdm(features_to_transform):
+                            min_neg_val = train[f].min()
+                            unary_transformation_model = FunctionTransformer(func=log_plus_const,
+                                                                             kw_args={'const': min_neg_val},
+                                                                             validate=True)
+                            train[f] = unary_transformation_model.fit_transform(
+                                X=np.array(train[f]).reshape(-1, 1))
+
+                        for f in tqdm(features_to_transform):
+                            min_neg_val = test[f].min()
+                            unary_transformation_model = FunctionTransformer(func=log_plus_const,
+                                                                             kw_args={'const': min_neg_val},
+                                                                             validate=True)
+                            test[f] = unary_transformation_model.fit_transform(
+                                X=np.array(test[f]).reshape(-1, 1))
+
+                    elif transformation == 'Sqrt':
+                        def sqrt_plus_const(x, const=0):
+                            return np.sqrt(x + np.abs(const) + 0.0001)
+
+                        for f in tqdm(features_to_transform):
+                            min_neg_val = train[f].min()
+                            unary_transformation_model = FunctionTransformer(func=sqrt_plus_const,
+                                                                             kw_args={'const': min_neg_val},
+                                                                             validate=True)
+                            train[f] = unary_transformation_model.fit_transform(
+                                X=np.array(train[f]).reshape(-1, 1))
+
+                        for f in tqdm(features_to_transform):
+                            min_neg_val = test[f].min()
+                            unary_transformation_model = FunctionTransformer(func=sqrt_plus_const,
+                                                                             kw_args={'const': min_neg_val},
+                                                                             validate=True)
+                            test[f] = unary_transformation_model.fit_transform(
+                                X=np.array(test[f]).reshape(-1, 1))
+                    else:
+                        unary_transformation_model = FunctionTransformer(func=np.square, validate=True)
+                        train[features_to_transform] = unary_transformation_model.fit_transform(
+                            X=train[features_to_transform])
+                        test[features_to_transform] = unary_transformation_model.transform(
+                            test[features_to_transform])
+            return train, test
+
+        train_set, test_set = compute_feature_importance(train=train_set, test=test_set, theta1=information_gain_thresh)
+        train_set, test_set = compute_feature_correlation(train=train_set, test=test_set, theta2=correlation_thresh)
+        train_set, test_set = apply_transform_fe(train=train_set, test=test_set)
+        return train_set, test_set
--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,6 +1,8 @@
 import sys
 import unittest
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 np.random.seed(7)
 sys.path.append('../')
 from interface.apis import KGFarm
@@ -28,3 +30,15 @@ class TestAPIs(unittest.TestCase):
 
         self.assertEqual(list(transformation_info['Recommended_transformation']),
                          expected_transformations)
+
+    def test_feature_engineering(self):
+        df = kgfarm.load_titanic_dataset()
+        df = df.dropna()
+        X = df.drop('Survived', axis=1)
+        y = df['Survived']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        train_set = pd.concat([X_train, y_train], axis=1)
+        test_set = pd.concat([X_test, y_test], axis=1)
+        train_set, test_set = kgfarm.engineer_features(train_set=train_set, test_set=test_set, target='Survived')
+        self.assertEqual(len(train_set.columns), len(test_set.columns))
